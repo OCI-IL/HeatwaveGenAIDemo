2b69c48d167ec58bde2540223a738f791a0a834b
Liron Sher <liron.sher@gmail.com>
2024-07-18 12:18:21 +0300

    add create env

---
+++ src/create_env.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Function to prompt the user for input and store it in a variable
+collect_input() {
+    local var_name=$1
+    local prompt_message=$2
+
+    read -p "$prompt_message" $var_name
+}
+
+# Static value for PORT
+PORT=3003
+
+# Collecting the required environment variables from the user
+collect_input DB_IP "Enter the DB_IP: "
+collect_input DB_USER "Enter the DB_USER: "
+collect_input DB_PASSWORD "Enter the DB_PASSWORD: "
+collect_input DB_NAME "Enter the DB_NAME: "
+collect_input REACT_APP_PAR "Enter the REACT_APP_PAR: "
+
+# Creating the .env file content for frontend and backend
+frontend_env_content="REACT_APP_PAR=${REACT_APP_PAR}"
+backend_env_content="PORT=${PORT}
+DB_IP=${DB_IP}
+DB_USER=${DB_USER}
+DB_PASSWORD=${DB_PASSWORD}
+DB_NAME=${DB_NAME}"
+
+# Define the directories
+frontend_dir="./frontend"
+backend_dir="./backend"
+
+# Save the .env file content to the frontend and backend directories
+echo "$frontend_env_content" > "$frontend_dir/.env"
+echo "$backend_env_content" > "$backend_dir/.env"
+
+# Confirmation message
+echo ".env file has been created in $frontend_dir and $backend_dir."
